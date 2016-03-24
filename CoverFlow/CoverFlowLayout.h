36d6b4f923b20c19ce33939f2aa0d5df9acce874
smollica <snmollic@hotmail.com>
2016-03-24 14:00:11 -0700

    final

---
+++ CoverFlow/CoverFlowLayout.h
@@ -0,0 +1,13 @@
+//
+//  CoverFlowLayout.h
+//  CoverFlow
+//
+//  Created by Monica Mollica on 2016-03-24.
+//  Copyright © 2016 Sergio Mollica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoverFlowLayout : UICollectionViewFlowLayout
+
+@end
---
+++ CoverFlow/CoverFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  CoverFlowLayout.m
+//  CoverFlow
+//
+//  Created by Monica Mollica on 2016-03-24.
+//  Copyright © 2016 Sergio Mollica. All rights reserved.
+//
+
+#import "CoverFlowLayout.h"
+
+@implementation CoverFlowLayout
+
+@end
