65c88911c92375b25fe645bcbff9c783aae50ad4
smollica <snmollic@hotmail.com>
2016-03-24 11:20:01 -0700

    start

---
+++ CoverFlow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoverFlow
+//
+//  Created by Monica Mollica on 2016-03-24.
+//  Copyright © 2016 Sergio Mollica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoverFlow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoverFlow
+//
+//  Created by Monica Mollica on 2016-03-24.
+//  Copyright © 2016 Sergio Mollica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
